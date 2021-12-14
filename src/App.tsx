3b8b7eb65cc18db3b47b7b131051e277b01d85ef
Jooyul Yoon <juyeolyoon@gmail.com>
2021-12-13 18:25:06 -0800

    type components by using interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import Circle from "./Components/Circle";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 function App() {
-  return <div />;
+  return (
+    <Wrapper>
+      <Circle bgColor="teal" />
+      <Circle bgColor="tomato" />
+    </Wrapper>
+  );
 }
 
 export default App;
